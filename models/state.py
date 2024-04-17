69390235ddeb7b516910d31f05301ff8534f77ee
Makarona10 <ahmedneyssi@gmail.com>
2024-04-18 00:52:31 +0200

    State fixes

--- models/state.py
+++ models/state.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import os
-from models import storage
 from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
+
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+    cities = relationship("City",
+                          backref="state", cascade="all, delete,delete-orphan")
 
-    if (os.getenv("HBNB_TYPE_STORAGE") != "db"):
+    if getenv('HBNB_TYPE_STORAGE') != "db":
         @property
         def cities(self):
-            """Returns list of cities related to state"""
-            cities = []
+            """ returns list of City instances related to state """
+            from models import storage
+            list_cities = []
             for city in storage.all(City).values():
                 if city.state_id == self.id:
-                    cities.append(city)
-            return cities
+                    list_cities.append(city)
+            return list_cities
