2068610dc2eb861641e45bca81b86058a9a6415c
Makarona10 <ahmedneyssi@gmail.com>
2024-04-18 00:50:38 +0200

    DB_Storage fixes

--- models/state.py
+++ models/state.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
-
+import os
+from models import storage
+from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",
-                          backref="state", cascade="all, delete,delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
 
-    if getenv('HBNB_TYPE_STORAGE') != "db":
+    if (os.getenv("HBNB_TYPE_STORAGE") != "db"):
         @property
         def cities(self):
-            """ returns list of City instances related to state """
-            from models import storage
-            list_cities = []
+            """Returns list of cities related to state"""
+            cities = []
             for city in storage.all(City).values():
                 if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+                    cities.append(city)
+            return cities
