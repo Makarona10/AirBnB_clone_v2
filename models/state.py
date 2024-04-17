c605813f1475f79515696f2c557c961830290c4a
Makarona10 <ahmedneyssi@gmail.com>
2024-04-18 01:09:43 +0200

    Some fixes

--- models/state.py
+++ models/state.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from os import getenv
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-import models
+"""This defines the State class."""
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",
-                          backref="state", cascade="all, delete,delete-orphan")
+class State(BaseModel):
+    """This represent a state.
 
-    if getenv('HBNB_TYPE_STORAGE') != "db":
-        @property
-        def cities(self):
-            """ returns list of City instances related to state """
-            from models import storage
-            list_cities = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
