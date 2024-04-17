301422e7ec017e7427dc5025e91be1491d0ec86f
Makarona10 <ahmedneyssi@gmail.com>
2024-04-18 01:11:48 +0200

    Some fixes

--- models/city.py
+++ models/city.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Defines the City class."""
-from models.base_model import BaseModel
+""" City Module for HBNB project """
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
-
-class City(BaseModel):
-    """This represent a city.
-
-    Attributes:
-        state_id (str): State id.
-        name (str): Name of the city.
-    """
-
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("state.id"), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-"""This defines the State class."""
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
+from models import storage
+from models.city import City
 
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
 
-class State(BaseModel):
-    """This represent a state.
-
-    Attributes:
-        name (str): The name of the state.
-    """
-
-    name = ""
+    if (os.getenv("HBNB_TYPE_STORAGE") != "db"):
+        @property
+        def cities(self):
+            """Returns list of cities related to state"""
+            cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
