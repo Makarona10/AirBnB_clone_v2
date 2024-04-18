c8c02a889157adaa4a24784f8593795fb0fccb09
Makarona10 <ahmedneyssi@gmail.com>
2024-04-18 13:39:47 +0200

    s

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 import models
+from models.review import Review
 from os import getenv
 
 class Place(BaseModel, Base):
