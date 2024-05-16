a4ac10231ab7975c67ae8933913f5ca03c68a24d
Makarona10 <ahmedneyssi@gmail.com>
2024-05-16 23:47:12 +0300

    First task done

---
+++ web_flask/test.py
@@ -0,0 +1,22 @@
+from flask import Flask, url_for, request, send_from_directory, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if (request.method == 'GET'):
+        return render_template('103-index.html')
+    else:
+        return "Say fuck CC!"
+
+@app.post('/<name>')
+def theName(name):
+    return f"Fuck {escape(name)}!"
+@app.get('/hello')
+def gethello():
+    return "Fuck Anyway"
+
+with app.test_request_context('/hello', method='GET'):
+    assert request.path == '/hello'
+    assert request.method == 'GET'
