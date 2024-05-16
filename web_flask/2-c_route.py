a3c3a4d696c997106ef16414658a1f301f7ac9a9
Makarona10 <ahmedneyssi@gmail.com>
2024-05-17 00:08:04 +0300

    Third file done

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,6 +4,7 @@
 
 
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -12,10 +13,17 @@ app = Flask(__name__)
 def home():
     """The homepage function"""
     return "Hello HBNB!"
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """The homepage function"""
     return "HBNB"
+
+
+@app.route('/c/<text>')
+def name(text):
+    return f"C {escape(text.replace('_', ' '))}!"
 
 
 if __name__ == '__main__':
