6c7674fbfc5a09f970091afc3ea1a13a060c7f78
Makarona10 <ahmedneyssi@gmail.com>
2024-05-19 00:58:13 +0300

    Eigth task done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""Script that runs a Flask web application"""
+
+"""A script that starts a Flask web application"""
+
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown(e):
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states():
-    """List all states"""
     states = storage.all("State")
-    # states_dic = {state['id']: state['name'] for state in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States", states=states)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
